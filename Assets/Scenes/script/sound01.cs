0ae58569f267ba95f0af3172683836cab9a80bbf
LYS93 <yslee93930@gmail.com>
2024-09-30 10:38:49 +0900

    09.30.10:38

--- Assets/Scenes/script/sound01.cs
+++ Assets/Scenes/script/sound01.cs
@@ -5,25 +5,35 @@ using UnityEngine.UI;
 
 public class sound01 : MonoBehaviour
 {
-    public AudioSource[] audioSources; // 여러 개의 AudioSource를 위한 배열
-    public Slider volumeSlider; // 연결할 Slider
-    public Text volumeText; // 현재 음량을 표시할 Text (선택적)
+    public AudioSource[] backgroundAudioSources; // 배경음 오디오 소스 배열
+    public AudioSource[] soundEffectAudioSources; // 효과음 오디오 소스 배열
+    public Slider backgroundVolumeSlider; // 배경음 슬라이더
+    public Slider soundEffectVolumeSlider; // 효과음 슬라이더
+    public Text backgroundVolumeText; // 배경음 현재 음량 텍스트
+    public Text soundEffectVolumeText; // 효과음 현재 음량 텍스트
 
     private void Start()
     {
-        // 슬라이더의 초기 값 설정
-        volumeSlider.value = audioSources[0].volume;
+        // 배경음 슬라이더 초기 값 설정
+        backgroundVolumeSlider.value = backgroundAudioSources[0].volume;
+        soundEffectVolumeSlider.value = soundEffectAudioSources[0].volume;
 
-        // 슬라이더 값이 변경될 때마다 모든 AudioSource의 음량 조정
-        volumeSlider.onValueChanged.AddListener(delegate {
-            for (int i = 0; i < audioSources.Length; i++)
+        // 배경음 슬라이더 값 변경 리스너
+        backgroundVolumeSlider.onValueChanged.AddListener(delegate {
+            for (int i = 0; i < backgroundAudioSources.Length; i++)
             {
-                audioSources[i].volume = volumeSlider.value;
+                backgroundAudioSources[i].volume = backgroundVolumeSlider.value;
             }
+            backgroundVolumeText.text = $"{Mathf.RoundToInt(backgroundVolumeSlider.value * 100)}%";
+        });
 
-            // 음량 텍스트 업데이트
-            if (volumeText != null)
-                volumeText.text = $"{Mathf.RoundToInt(volumeSlider.value * 100)}%";
+        // 효과음 슬라이더 값 변경 리스너
+        soundEffectVolumeSlider.onValueChanged.AddListener(delegate {
+            for (int i = 0; i < soundEffectAudioSources.Length; i++)
+            {
+                soundEffectAudioSources[i].volume = soundEffectVolumeSlider.value;
+            }
+            soundEffectVolumeText.text = $"{Mathf.RoundToInt(soundEffectVolumeSlider.value * 100)}%";
         });
     }
 }
