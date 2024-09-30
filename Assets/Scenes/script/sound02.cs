1526ac0320672f4b7278a02a7611b6395a799c33
LYS93 <yslee93930@gmail.com>
2024-09-30 13:18:33 +0900

    09.30.13:18

---
+++ Assets/Scenes/script/sound02.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sound02 : MonoBehaviour
+{
+    private void Awake()
+    {
+        // 이 오브젝트가 파괴되지 않도록 설정
+        DontDestroyOnLoad(gameObject);
+    }
+
+}
