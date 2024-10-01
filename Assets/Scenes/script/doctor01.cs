15208327e1bb261e98a2ad8945313c3c791699b6
LYS93 <yslee93930@gmail.com>
2024-10-01 15:45:34 +0900

    시월의 첫날에 코드를 올려본다

---
+++ Assets/Scenes/script/doctor01.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class doctor01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //사용자가 스페이스바키를 눌렀을 때
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Destroy(gameObject, 0.5f); // 이 GameObject를 파괴
+        }
+    }
+}
